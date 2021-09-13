14100e1d0906aed0d4be121f25ef17fc834d8a7f
neckita39 <84981226+neckita39@users.noreply.github.com>
2021-09-13 22:58:12 +0200

    апдейт

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,33 +5,31 @@ public class Main {
 
         try {
             System.out.println("Книги:");
-            Biblio<Book> bibB = new Biblio(2);
+            Biblio<Book> bibB = new Biblio();
             Book b = new Book("Война и Мир том первый");
             Book b1 = new Book("Война и мир том второй");
             bibB.add(b);
             bibB.add(b1);
-            for (int i = 0; i < bibB.size(); i++) {
+            for (int i = 0; i < 2; i++) {
                 System.out.println(bibB.get(i).getName());
             }
             System.out.println("Фильмы: ");
-            Biblio<Video> bibV = new Biblio(2);
+            Biblio<Video> bibV = new Biblio();
             Video v = new Video("Криминальное Чтиво");
             Video v1 = new Video("Большой Лебовски");
             bibV.add(v);
             bibV.add(v1);
             System.out.println("Первый фильм из списка - " + bibV.get(0).getName());
             System.out.println("Газеты: ");
-            Biblio<Magazine> bibM = new Biblio(2);
+            Biblio<Magazine> bibM = new Biblio();
             Magazine m = new Magazine("Комсомольская правда");
             bibM.add(m);
-            bibM.add(m);
-            for (int i = 0; i < bibM.size(); i++) {
-                System.out.println(bibM.get(i).getName());
-            }
-            bibM.remove(1);
-            for (int i = 0; i < bibM.size(); i++) {
-                System.out.println(bibM.get(i).getName());
-            }
+
+
+            System.out.println(bibM.get(0).getName());
+
+            bibM.remove(0);
+            System.out.println(bibM.get(0).getName());
         }catch (Exception ex){
             System.out.println(ex.getMessage());
         }
--- src/main/java/ru/biblio/Biblio.java
+++ src/main/java/ru/biblio/Biblio.java
@@ -5,28 +5,25 @@ import java.util.List;
 
 public class Biblio<T> {
     private List<T> els=new ArrayList<>();
-    private int s;
 
-    public Biblio(int s){
-        this.s=s;
-        els=new ArrayList<T>(s);
+
+    public Biblio(){
+
     }
     public void add(T obj){
         this.els.add(obj);
     }
     public void remove(int i) throws Exception{
-        if(!els.isEmpty())
+        if(i>=0 && i<els.size())
             this.els.remove(i);
         else throw new Exception("Список пуст");
     }
     public T get(int i) throws Exception {
-            if (i >= 0 && i < s)
+            if (i >= 0 && i < els.size())
                 return els.get(i);
             else throw new Exception("Неверный индекс");
     }
-    public int size(){
-        return s;
-    }
+
 
 
 
